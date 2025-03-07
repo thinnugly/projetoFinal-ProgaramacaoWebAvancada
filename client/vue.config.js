c62ae29e02ef1774fb5575c28db133376285c632
thinnugly <amlieogalaxias@gmail.com>
2025-03-07 17:14:24 +0200

    Adjust vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,15 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.BASE_URL || '/', 
-  outputDir: 'dist', 
-  assetsDir: 'assets',
-  indexPath: 'index.html', 
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Backend local
-        changeOrigin: true,
-      },
-    },
-  },
 });
