bc9738bfe49a3b39dc3b57630823b1808eb7b91c
thinnugly <amlieogalaxias@gmail.com>
2025-03-07 10:18:54 +0200

    Adjust on Middleware

--- server/init/middleware.js
+++ server/init/middleware.js
@@ -4,15 +4,14 @@ module.exports = (app) => {
     const cors = require('cors');
     app.use(cors());
     app.options('*', cors());
-    app.use((req, res, callback) => {
+    app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods',
-            'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.header('Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Authorization, Language, Location');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Language, Location');
         res.header('Access-Control-Expose-Headers', 'Authorization, Language, Location');
-        return callback();
-    })
+        next(); 
+    });
+    
     app.use(bodyParser.urlencoded({
         extended: true
     }));
